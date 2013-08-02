5f7181d2be3aee930f3df9fbafaed5bb3f8b86d5
Ignazio Setti <ignazio.setti@gmail.com>
2013-08-02 02:05:05 +0200

    Remove credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, 'f771253f32d8583acdb8', '1120eb8c617550aa66599b6716f99a86bc2277a3', scope: 'user'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user'
 end
